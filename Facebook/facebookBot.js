ea4fa69d640b4eb9664073bce0052159e7ba9614
jhans <jhansnicolas@gmail.com>
2023-02-06 16:57:17 -0500

    agregamos quick replies

--- Facebook/facebookBot.js
+++ Facebook/facebookBot.js
@@ -152,6 +152,22 @@ async function handleDialogFlowAction(
   parameters
 ) {
   switch (action) {
+    case "Codigo.quickReply.action":
+      sendQuickReply(sender, "Ejemplo de quick reply", [
+        {
+          payload: "si_acepto",
+          image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Yes_Check_Circle.svg/1024px-Yes_Check_Circle.svg.png",
+          content_type: "text",
+          title: "Si",
+        },
+        {
+          image_url: "https://static.vecteezy.com/system/resources/previews/000/501/351/non_2x/vector-cancel-icon-design.jpg",
+          payload: "<POSTBACK_PAYLOAD>",
+          content_type: "text",
+          title: "No",
+        },
+      ]);
+      break;
     default:
       //unhandled action, just send back the text
       handleMessages(messages, sender);
