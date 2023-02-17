6c22382c82a5864187ba102103f332ce1fd6a316
jhans <jhansnicolas@gmail.com>
2023-02-17 16:47:31 -0500

    creamos routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/chatbot", (req, res) => {
     res.json({ ok: true, msg: "Esto esta funcionando bien" });
 });
 
